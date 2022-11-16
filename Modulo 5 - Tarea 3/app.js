3b00073fd858e22a74107aaa3665eeaef9bc8f92
Sebastián De Liberti <sebastiandeliberti@gmail.com>
2022-11-16 06:29:25 -0300

    Tarea módulo 5 unidad 3

---
+++ Modulo 5 - Tarea 3/app.js
@@ -0,0 +1,107 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// Base de datos
+
+require('dotenv').config();
+
+var pool = require('./models/bd');
+
+///////////////////////////////////
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Consultas
+
+// Select
+/*
+pool.query('select * from empleados').then(function (resultados) {
+
+  console.log(resultados);
+
+});
+*/
+// Insert
+/*
+var obj = {
+
+  nombre: 'Edgar',
+  apellido: 'Correa',
+  trabajo: 'Pescador',
+  edad: 41,
+  salario: 21430,
+  mail: 'edgarcorrea@gmail.com'
+
+}
+
+pool.query('insert into empleados set ?', [obj]).then (function (resultados) {
+
+  console.log(resultados);
+
+});
+*/
+// Modif
+/*
+var id = 16;
+
+var obj = {
+
+  nombre: 'Monica',
+  apellido: 'Lanchez'
+
+}
+
+pool.query('update empleados set ? where id_emp=?', [obj,  id]).then(function (resultados) {
+
+  console.log(resultados);
+
+});
+*/
+// Delete
+/*
+var id = 17;
+
+pool.query('delete from empleados where id_emp=?', [id]).then(function (resultados) {
+
+  console.log(resultados);
+
+});
+*/
+////////////////////////////////////////
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
